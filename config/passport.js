aba5372d9c7c86982cb80c7be87ba4b5d5d46a5d
Bwiza-Nina <ninamyles2020@gmail.com>
2022-01-19 14:42:49 +0200

    login with google

--- config/passport.js
+++ config/passport.js
@@ -6,6 +6,41 @@ passport.use(
     new GoogleStrategy({
         clientID:"",
         clientSecret: "",
-        callBackUrl: ""
-    }, )
-)
+        callbackUrl: ""
+    }, 
+    async (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+        //get the user data from Google
+        const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.firstName,
+            lastName: profile.lastName,
+            image: profile.photos[0].value,
+            email: profile.emails[0].value
+        }
+        console.log(newUser);
+        try{
+            //finding the user in the database
+            let user = await GoogleAccount.findOne({googleId: profile.id});
+            if(user){
+                //if present in the database
+                done(null, user);
+            }else {
+                user = await GoogleAccount.create(newUser);
+                done(null, user);
+            }
+        }catch(error){
+            console.log(error)
+        }
+    }
+));
+//used to serialize the user from the session
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+//deserialize the user
+passport.deserializeUser((id, done) => {
+    GoogleAccount.findById(id, (err, user) => done(err, user));
+});
+modoule.exports.Passport = passport
